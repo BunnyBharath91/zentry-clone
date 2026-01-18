30031c258fb0e035c1aa7227ab57818fe4677988
BunnyBharath91 <bunnybharath917@gmail.com>
2026-01-18 22:22:23 +0530

    setup tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-violet-300 text-5xl font-bold">Welcome to Awwards</h1>
+    </main>
+  );
+};
+
+export default App;
