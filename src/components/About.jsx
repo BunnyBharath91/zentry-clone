5f19a5a7a2241f11167ffc16d0f363481f256f49
BunnyBharath91 <bunnybharath917@gmail.com>
2026-01-19 06:12:54 +0530

    Fix className

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
 
   return (
     <div id="about" className="min-h-screen w-screen">
-      <div class="relative mb-8 mt-36 flex flex-col items-center gap-5">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </h2>
