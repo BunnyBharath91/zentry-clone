c7068fda301f83f5641837e46af6427db7ed950d
BunnyBharath91 <bunnybharath917@gmail.com>
2026-01-19 15:51:14 +0530

    Update audio icon styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
       id={id}
       className={clsx(
         "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black",
-        containerClass
+        containerClass,
       )}
     >
       {leftIcon}
