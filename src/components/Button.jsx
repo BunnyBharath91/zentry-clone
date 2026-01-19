06968b9286a6dbcc5d85c116a10055e2d4466b20
BunnyBharath91 <bunnybharath917@gmail.com>
2026-01-19 06:05:03 +0530

    Create Contact and Footer sections

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Button = (props) => {
+  const { title, id, rightIcon, leftIcon, containerClass } = props;
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-poitner overflow-hidden
+         rounded-full bg-violet-50 px-7 py-3 text-black 
+         ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+      {rightIcon}
+    </button>
+  );
+};
+
+export default Button;
