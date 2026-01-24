2dcf0a9f2c35b1b326f1ee231e878e6306955106
BunnyBharath91 <bunnybharath917@gmail.com>
2026-01-24 19:35:08 +0530

    Update portfolio link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { FaTwitter, FaLink, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const socialLinks = [
-  { href: "https://bharath-kumar-borra.onrender.com", icon: <FaLink /> },
+  { href: "https://bharath-kumar-borra.vercel.app", icon: <FaLink /> },
   { href: "https://www.linkedin.com/in/bharathkumarb1", icon: <FaLinkedin /> },
   { href: "https://x.com/BharathKumar960", icon: <FaTwitter /> },
   { href: "http://instagram.com/__bunny_bharath_", icon: <FaInstagram /> },
@@ -12,7 +12,14 @@ const Footer = () => {
     <footer className="w-screen bg-black py-4 text-white">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          Built by <a href="https://bharath-kumar-borra.vercel.app" target="_blank" rel="noopener noreferrer">Bharath Kumar Borra</a>
+          Built by{" "}
+          <a
+            href="https://bharath-kumar-borra.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bharath Kumar Borra
+          </a>
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
